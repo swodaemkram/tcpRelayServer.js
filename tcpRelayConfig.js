acecee279ffdbd92cf306d0d7bb5d8c392393584
Mark Meadows <42901465+swodaemkram@users.noreply.github.com>
2019-01-30 10:47:50 -0500

    Update tcpRelayConfig.js

--- tcpRelayConfig.js
+++ tcpRelayConfig.js
@@ -3,25 +3,19 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-/**
- *  Copyright Surveillus Networks LLC 2018
- *
- *  TCP relay configuration example file
- */
 
-// in a real deployment scenario, pull all these settings from environment variables
 exports.default = {
 
     // listen on this port
     port: 32011,
 
     destinations: [
-    // forward to existing ImageVault system
+    // forward to Host 1
     {
         host: '10.1.1.99',
         port: 32011
     },
-    // also forward to local blackbox
+    // also forward to Host 2
     {
         host: '127.0.0.1',
         port: 12001
